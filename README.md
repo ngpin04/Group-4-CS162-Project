4d36f2bb5a16c78582f11400db8bac2a7219a40e
mdkhoat2 <mdkhoat2@gmail.com>
2023-03-18 23:38:28 +0700

    added struct for user

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Group-4-CS162-Project
+mdkhoat2 committed
---
+++ header/user.h
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct date{
+    int day;
+    int month;
+    int year;
+};
+
+struct user
+{
+    int id; //student or staff id
+    bool isStaff = false; //default as student
+    date birth;
+    string username;
+    string password = "KHTN123@"; //the dafault password need to be set to
+    void changePassword();
+};
