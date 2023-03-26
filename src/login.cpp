f979fb43ace162ca9cf298077e5697fc6f1ef898
Bình Nguyễn <ngpin04@Binhs-MacBook-Air.local>
2023-03-26 22:00:23 +0700

    fix clearScreen function

--- src/login.cpp
+++ src/login.cpp
@@ -2,13 +2,18 @@
 #include "../header/user.h"
 using namespace std;
 
-void clearScreen() {
-if (_WIN32)
-    system("cls");
-else
-    system("clear");
+void clearScreen()
+{
+    #if defined _WIN32
+        system("cls");
+        //clrscr(); // including header file : conio.h
+    #elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+        system("clear");
+        //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+    #elif defined (__APPLE__)
+        system("clear");
+    #endif
 }
-
 void checkUserAtLogIn(userList *pHead, user *&curUser)
 {
     clearScreen();
