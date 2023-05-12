079ad51e9262cffc294cf3b184284cf74fc73d23
Bình Nguyễn <binhnp.ti.1922@gmail.com>
2023-05-12 19:36:43 +0700

    fix deleteCourse.cpp

--- src/deleteCourse.cpp
+++ src/deleteCourse.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void deleteCourse(courseList *courses) {
+void deleteCourse(courseList *&courses) {
     if (courses == nullptr) {
         cout << "There is no courses to update! Press any key to return" << endl;
         cin.ignore();
@@ -11,20 +11,35 @@ void deleteCourse(courseList *courses) {
         return;
     }
     string id; 
-    cout << " Please enter the course id that you want to delete: ";
-    cin >> id;
-    courseList *pre = nullptr;
+    cout << " Please choose the course that you want to delete: ";
+    int count = 0;
     for (courseList *cur = courses; cur != nullptr; cur = cur->next) {
-        if (cur->data.id == id) {
-            if (pre == nullptr)
-                courses = cur->next;
-            else 
-                pre->next = cur->next;
-            cout << " Courses deletion is done!" << endl;
-            delete cur;
-            return;
-        }
+        count++;
+        cout << count << ". " << cur->data.id << " " << cur->data.className << "\n";
+    }
+
+    int res; cin >> res;
+    if (res > count || res <= 0) {
+        cout << "Invalid choice! Press any key to go back\n";
+        cin.ignore();
+        cin.get();
+        return;
+    }
+    
+    courseList *pre = nullptr, *cur = courses;
+    res--;
+    while (res--) {
         pre = cur;
+        cur = cur->next;
+    }
+
+    if (pre == nullptr){
+        courses = cur->next;
+    } else {
+        pre->next = cur->next;
+        cout << " Courses deletion is done!" << endl;
+        delete cur;
+        return;
     }
     cout << " There is no course with the provided id!" << endl;
 }
